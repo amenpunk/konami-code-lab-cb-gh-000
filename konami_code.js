3616fb572029c50bd0f6f7d052382f784b76f391
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-29 02:39:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   const body = document.querySelector('body');
   body.addEventListener("keydown", function (event) {
   //console.log(event.key);
-  key = event.key;
+  const key = event.key;
   if(key === code[index]){
     index++
     if(index === code.lenght){
